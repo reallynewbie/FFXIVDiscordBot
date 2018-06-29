c6868081ccc395a016574023617dba0b64acdf55
reallynewbie <bruceleung@shaw.ca>
2018-06-28 18:35:43 -0600

    Add mongoose support

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+var mongoose = require('mongoose');
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use(cors());
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(3000);
