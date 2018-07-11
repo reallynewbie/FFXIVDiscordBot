aebe92bd122b1d7b7773197dbec05d549b7f96f2
reallynewbie <bruceleung@shaw.ca>
2018-07-11 17:51:16 -0600

    Add Mongoose Testing

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 const app = express();
 
+const Account = require("./models/Account");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/my_database';
+var mongoDB = "mongodb://127.0.0.1/my_database";
 mongoose.connect(mongoDB);
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
@@ -17,6 +18,36 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function() {
+  var testAccount = new Account({
+    username: "test",
+    password: "test",
+    discordName: "testDiscord",
+    accountID: 5
+  });
+  var promise = Account.find(function(err, accounts) {
+    if (err) return console.error(err);
+    console.log("Accounts found: ", accounts);
+  });
+  promise.then(function() {
+    promise = testAccount.save(function(err, res) {
+      if (err) return console.error(err);
+      console.log("Save completed");
+    });
+  });
+  promise.then(function() {
+    promise = Account.find(function(err, accounts) {
+      if (err) return console.error(err);
+      console.log("Accounts found: ", accounts);
+    });
+  }).then(function() {
+    Account.remove({}, function(err) {
+      console.log("collection removed");
+    });
+  });
 
-app.listen(3000);
+  
+});
+
+console.log("completed");
