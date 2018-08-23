6a142add80bb0136c49a3336793a61eaf569e2b0
reallynewbie <bruceleung@shaw.ca>
2018-08-23 17:00:48 -0600

    Scale down extraneous imports and have working AddAccount

--- mongoosedb.js
+++ mongoosedb.js
@@ -1,15 +1,9 @@
 require("dotenv").config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 var mongoose = require("mongoose");
-const app = express();
+const autoIncrement = require("mongoose-auto-increment");
 
 const Accounts = require("./models/Account");
-
-app.use(bodyParser.json());
-app.use(cors());
 
 //Set up default mongoose connection
 var mongoDB = "mongodb://127.0.0.1/my_database";
@@ -19,43 +13,69 @@ mongoose.Promise = global.Promise;
 //Get the default connection
 var db = mongoose.connection;
 
+//Init Autoincrement for field accountID and for a model named Accounts.
+autoIncrement.initialize(db);
+Accounts.schema.plugin(autoIncrement.plugin, {
+  model: "Accounts",
+  field: "accountID"
+})
+
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // var accountSchema = new Schema({
 //   discordName: String,
 //   discordAcctID: String,
-//   accountID: Number,
-//   character: WoL,
+//   accountID: Number, (Filled with autoincrement)
 //   updateffLogsTrigger: {type: Boolean, default: false}
 // });
 
-async function addAccount(acctInfo) {
-  console.log("Start addAccount:  ", acctInfo);
+function addAccount(acctInfo) {
+  //Error Checking for acctInfo, returns promise
+  let acctCheck = checkNewAccount(acctInfo);
 
-  //Check if account exists
-  try {
-    let discordID = acctInfo.discordAcctID;
-    findAccount(discordID, function(err, result) {
-      if (result.length != 0) {
-        console.log("Result = ", result);
+  acctCheck.then(success => {
+    try {
+      let discordID = acctInfo.discordAcctID;
+      findAccount(discordID, function (err, result) {
+        if (result.length != 0) {
+          console.log("Result = ", result);
+        } else {
+          newAccount = new Accounts.model(acctInfo);
+          newAccount.save(function (err, res) {
+            if (err) return console.error(err);
+            console.log("Save completed");
+          });
+        }
+      });
+    } catch (err) {
+      if (err.name == "ReferenceError") {
+        console.log("discordAcctID doesn't exist", err);
+      } else console.log(err);
+    }
+  }, reject => {
+    console.log(reject);
+  });
+}
+
+function checkNewAccount(acctInfo) {
+  return new Promise((resolve, reject) => {
+    if (acctInfo.discordAcctID !== undefined) {
+      if (acctInfo.discordName !== undefined) {
+        resolve(true);
       } else {
-        newAccount = new Accounts(acctInfo);
-        newAccount.save(function(err, res) {
-          if (err) return console.error(err);
-          console.log("Save completed");
-        });
+        reject("undefined discordName");
       }
-    });    
-  } catch (err) {
-    if (err.name == "ReferenceError") {
-      console.log("discordAcctID doesn't exist", err);
-    } else console.log(err);
-  }
+    } else {
+      reject("undefined discordAcctID");
+    }
+  })
 }
 
 function findAccount(acctID, callback) {
-  Accounts.find({ discordAcctID: "acctID" }, "discordName", function(
+  Accounts.model.find({
+    discordAcctID: acctID
+  }, "discordName", function (
     err,
     account
   ) {
@@ -71,36 +91,8 @@ module.exports = {
   findAccount,
   deleteAccount
 };
-
-// db.once("open", function() {
-//   var testAccount = new Account({
-//     username: "test",
-//     password: "test",
-//     discordName: "testDiscord",
-//     accountID: 5
-//   });
-//   var promise = Account.find(function(err, accounts) {
-//     if (err) return console.error(err);
-//     console.log("Accounts found: ", accounts);
-//   });
-//   promise.then(function() {
-//     promise = testAccount.save(function(err, res) {
-//       if (err) return console.error(err);
-//       console.log("Save completed");
-//     });
-//   });
-//   promise
-//     .then(function() {
-//       promise = Account.find(function(err, accounts) {
-//         if (err) return console.error(err);
-//         console.log("Accounts found: ", accounts);
-//       });
-//     })
-//     .then(function() {
-//       Account.remove({}, function(err) {
-//         console.log("collection removed");
-//       });
-//     });
-// });
-
+addAccount({
+  discordAcctID: "",
+  discordName: ""
+}) ;
 console.log("Mongoosedb Completed");
