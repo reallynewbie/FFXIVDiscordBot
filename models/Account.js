fde20e3cf72cbc108b2bbfdde06e308f30e911b1
reallynewbie <bruceleung@shaw.ca>
2018-08-23 15:07:57 -0600

    Update Account to export both Model and Schema

--- models/Account.js
+++ models/Account.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+
 //const WoL = require("./Character");
 // Define schema
 var Schema = mongoose.Schema;
-//Took out char:character: WoL,
+
 var accountSchema = new Schema({
-    discordName: String,
-    discordAcctID: String,
-    accountID: Number,
-    updateffLogsTrigger: {type: Boolean, default: false}
+  discordName: String,
+  discordAcctID: String,
+  accountID: Number,
+  updateffLogsTrigger: { type: Boolean, default: false }
 });
-
-
 
 // Compile model from schema
 var Account = mongoose.model("Account", accountSchema);
 
-module.exports = Account;
+module.exports = {
+  schema: accountSchema,
+  model: Account
+};
