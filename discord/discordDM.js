3f67be234b5cc3ad4ed415749ef013150e84d77f
reallynewbie <bruceleung@shaw.ca>
2018-09-14 18:02:31 -0600

    Add Stats command and begin FFLogs Update Command

--- discord/discordDM.js
+++ discord/discordDM.js
@@ -4,10 +4,20 @@ updatefflogs - *Links your account with a new FFLogs Account*
 char/me/stats - *Displays your current stats*`;
 
 const fflogsString = `__**Link FFLogs Account**__
-*Copy and paste a link to your FFLogs page*`
+*Copy and paste a link to your FFLogs page*
+ex:  https://www.fflogs.com/character/na/diabolos/really%20newbie
+Note:  Linking via ID number page isn't supported yet (https://www.fflogs.com/character/id/207082)`
 
+const fflogsUpdateStr = `__**Update FFLogs Account**__
+*Copy and paste a link to your new associated FFLogs Page*
+ex:  https://www.fflogs.com/character/na/diabolos/really%20newbie
+Note:  Linking via ID number page isn't supported yet (https://www.fflogs.com/character/id/207082)`
+
+
+const Discord = require("discord.js");
 const db = require("../mongoosedb.js");
 const charController = require("../controllers/charController.js");
+const filter = m => !m.author.bot;
 
 module.exports = function (client, inmsg) {
   let args = parseMessage(inmsg);
@@ -42,7 +52,7 @@ module.exports = function (client, inmsg) {
       } else {
         throw new Error("more or less than one account found in findAccount");
       }
-    }).then(confirmedAccount => {
+    }).then(async confirmedAccount => {
       console.log("IN THEREEEE", confirmedAccount);
       //No Collector Active, proceeed with normal parse of command
       switch (command) {
@@ -51,12 +61,21 @@ module.exports = function (client, inmsg) {
           break;
         case "update":
         case "updatefflogs":
+          inmsg.channel.send(fflogsUpdateStr);
+          inmsg.channel.awaitMessages(filter, {
+              max: 1
+            })
+            .then(collected => {
+              checkFFURL(collected.first());
+            })
           updateFFLogs(inmsg);
           break;
         case "char":
         case "mychar":
         case "stats":
-          inmsg.channel.send("You called for char?");
+          let charInfo = await charController.findChar(inmsg.author.id);
+          let statsMsg = embeddedStatsCreator(charInfo.charFName, charInfo.charLName, charInfo.jobs);
+          inmsg.channel.send(statsMsg);
           break;
         case "createchar":
 
@@ -64,7 +83,6 @@ module.exports = function (client, inmsg) {
 
           //If account not there, display Text to guide user to paste in the fflogs link next.
           inmsg.channel.send(fflogsString);
-          const filter = m => !m.author.bot;
           inmsg.channel.awaitMessages(filter, {
               max: 1
             })
@@ -95,6 +113,9 @@ module.exports = function (client, inmsg) {
               throw (err);
             })
           break;
+        case "deletechar":
+
+          break;
         default:
           break;
       }
@@ -105,6 +126,33 @@ module.exports = function (client, inmsg) {
 };
 
 function updateFFLogs(message) {}
+
+function embeddedStatsCreator(fName, lName, jobArray) { //This is bad, don't send an entire object.  Send parameters separated.
+  if (jobArray.length == 0) {
+    //Show custom message
+  } else {
+    let finalMsg = new Discord.RichEmbed();
+    finalMsg.setTitle("__**" + fName + " " + lName + "**__");
+
+    jobArray.forEach(element => {
+      finalMsg.addField(element.job, element.exp, true);
+    });
+    return finalMsg;
+  }
+}
+
+function checkFFURL(url) {
+  if (url.content.toLowerCase().startsWith("https://www.fflogs.com/character")) {
+    if (url.content.toLowerCase().startsWith("https://www.fflogs.com/character/id")) {
+      //Scrape the webpage to get char name and server + region. In the future
+      console.error("Using ID, not yet supported", response)
+      throw new Error("Using ID, not yet supported");
+    }
+    else {
+      return true;
+    }
+  }
+}
 
 function parseMessage(msg) {
   return msg.content.trim().split(/ +/g);
